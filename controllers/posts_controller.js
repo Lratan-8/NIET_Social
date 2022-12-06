bf084a01e7d823c9e3c1a3c6fe56b372d01c0739
Luv <luv.ratan@indegene.com>
2022-12-06 23:12:56 +0530

    users/posts page added

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 module.exports.usersPosts = function(req,res){
 
-    return res.end('<h1>This is users/posts controller</h1>');
+    // return res.end('<h1>This is users/posts controller</h1>');
+    return res.render('posts')
 
 };
